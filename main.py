bf39c757bfebcf7b4799362daae5abd90b0e4bb9
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2023-02-14 15:38:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import os
 os.system("clear")
-os.system("figlet                     NETT")
-print("Repository:        https://github.com/Fandi63/NETT")
-print("Options:")
-print("                      [1] Test the wifi card")
-print("                      [2] Ping google")
-print("                      [3] Ping microsoft")
-input("nett>")
-if input() is 1:
-  os.system ("airmon-ng start wlan0")
-if input() is 2:
-  os.system("ping www.google.com")
-if input() is 3:
-  os.system("ping www.microsoft.com")
+os.system("figlet  checkTool")
+print("Repository: https://github.com/Fandi63/checktool")
+input("ct> ")
+os.system ("airmon-ng start wlan0")
   
